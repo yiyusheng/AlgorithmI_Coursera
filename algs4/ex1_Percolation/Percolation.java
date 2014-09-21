ab100309ba12a240f5539116a76a118faed26558
yiyusheng <yiyusheng.hust@gmail.com>
2014-09-21 21:53:10 +0800

    092103

--- algs4/ex1_Percolation/Percolation.java
+++ algs4/ex1_Percolation/Percolation.java
@@ -4,58 +4,62 @@ public class Percolation {
  private int[][] state;
  private QuickFindUF p;
 
-    public Percolation(int N)
-   {             // create N-by-N grid, with all sites blocked
-  if (N<=0) {
+ public Percolation(int N)
+ {             // create N-by-N grid, with all sites blocked
+  if (N <= 0) {
    throw new IllegalArgumentException(); 
   }
-  p=new QuickFindUF(N*N);
-  len=N;
-  int i,j;
-  for(i=0;i<N;i++){
-   for(j=0;j<N;j++){
-    state[i][j]=0;
+  p = new QuickFindUF(N*N);
+  len = N;
+  int i, j;
+  state = new int[N][N];
+  for (i = 0; i < N; i++) {
+   for (j = 0; j < N; j++) {
+    state[i][j] = 0;
    }
   }
-    }
+ }
 
 
-    public void open(int i, int j)   {        // open site (row i, column j) if it is not already
-  if (i<1 || i>len ||j<1 || j>len) {
+ public void open(int i, int j)  
+ {
+// open site (row i, column j) if it is not already
+  if (i < 1 || i > len || j < 1 || j > len) {
    throw new java.lang.IndexOutOfBoundsException();
   }
-  state[i-1][j-1]=1;
-  int m=(i-1)*len+j-1;
-  int mup=(i-2)*len+j-1;
-  int mdown=i*len+j-1;
-  int mleft=(i-1)*len+j-2;
-  int mright=(i-1)*len+j;
-  if (i!=1 && isOpen(i-1,j))
-   p.union(m,mup);
-  if (i!=len && isOpen(i+1,j))
-   p.union(m,mdown);
-  if (j!=1 && isOpen(i,j-1))
-   p.union(m,mleft);
-  if (j!=len && isOpen(i,j+1))
-   p.union(m,mright);
+  state[i-1][j-1] = 1;
+
+  int m = (i-1)*len+j-1;
+  int mup = (i-2)*len+j-1;
+  int mdown = i*len+j-1;
+  int mleft = (i-1)*len+j-2;
+  int mright = (i-1)*len+j;
+  if (i != 1 && isOpen(i-2, j-1))
+   p.union(m, mup);
+  if (i != len && isOpen(i, j-1))
+   p.union(m, mdown);
+  if (j != 1 && isOpen(i-1, j-2))
+   p.union(m, mleft);
+  if (j != len && isOpen(i-1, j))
+   p.union(m, mright);
     }
 
 
     public boolean isOpen(int i, int j)   {   // is site (row i, column j) open?
-  if (i<1 || i>len ||j<1 || j>len) {
+  if (i < 1 || i > len || j < 1 || j > len) {
    throw new java.lang.IndexOutOfBoundsException();
   }
-        return state[i-1][j-1]==1;
+        return state[i-1][j-1] == 1;
     }
 
 
     public boolean isFull(int i, int j)    {  // is site (row i, column j) full?
-  if (i<1 || i>len ||j<1 || j>len) {
+  if (i < 1 || i > len || j < 1 || j > len) {
    throw new java.lang.IndexOutOfBoundsException();
-  }else {
-   int m=(i-1)*len+j-1;
-   for(i=0;i<len;i++) {
-       if (p.connected(i,m))
+  } else {
+   int m = (i-1)*len+j-1;
+   for (int k = 0; k < len; k++) {
+       if (p.connected(k, m))
      return true;
    }
    return false;
@@ -63,14 +67,15 @@ public class Percolation {
     }
 
 
-    public boolean percolates()     {         // does the system percolate?
-  int i;
-  for (i=0;i<len;i++) {
-   if (isFull(len,i+1))
-    return true;
+public boolean percolates() {         // does the system percolate?
+  for (int i = 0; i < len; i++) {
+   for (int j = 0; j < len; j++) {
+    if (isFull(i, (len-1)*len+j))
+     return true;
+   }
   }
   return false;
-    }
+}
 
 
     public static void main(String[] args)  { // test client, optional
--- algs4/ex1_Percolation/PercolationStats.java
+++ algs4/ex1_Percolation/PercolationStats.java
@@ -1,51 +1,54 @@
 public class PercolationStats {
-	private	double[] count_array;
-	private int ipt_T;
+   private double[] arraySta;
+   private int ipt;
    public PercolationStats(int N, int T)
-   {	   // perform T independent computational experiments on an N-by-N grid
-	   if (N<=0 || T<=0) throw new IllegalArgumentException();
-	   count_array=new double[T];
-	   ipt_T=T;
-	   int[] tag=new int[N*N];
-	   for (int i=0;i<N*N;i++)
-		   tag[i]=i;
-	   for (int i=0;i<T;i++) 
-	   {
-		   int [] curr_tag=tag;
-		   Percolation p=new Percolation(N);
-		   StdRandom.setSeed(i+89757);
-		   StdRandom.shuffle(curr_tag);
-		   int j;
-		   for (j=0;j<N*N;j++)
-		   {
-			   int curr_r=curr_tag[j];
-			   int curr_row=curr_r/N+1;
-			   int curr_column=curr_r%N+1;
-			   p.open(curr_row,curr_column);
-			   if (p.percolates())
-				   break;
-		   }
-		   count_array[i]=(j+1)/N/N;
-		   System.out.print(count_array[i]);
-	   }
+   {
+// perform T independent computational experiments on an N-by-N grid
+     if (N <= 0 || T <= 0) throw new IllegalArgumentException();
+     arraySta = new double[T];
+     ipt = T;
+     int[] tag = new int[N*N];
+     for (int i = 0; i < N*N; i++)
+       tag[i] = i;
+     for (int i = 0; i < T; i++) 
+     {
+       int [] ctag = tag;
+       Percolation p = new Percolation(N);
+       StdRandom.setSeed(i + 89757);
+       StdRandom.shuffle(ctag);
+       int j;
+       for (j = 0; j < N*N; j++)
+       {
+         int cr = ctag[j];
+         int crow = cr/N+1;
+         int ccolumn = cr % N+1;
+         p.open(crow, ccolumn);
+         if (p.percolates())
+           break;
+       }
+       arraySta[i] = (j+1)/N/N;
+       System.out.print(arraySta[i]);
+     }
    }
    public double mean()
    {                     // sample mean of percolation threshold
-	   return StdStats.mean(count_array);
+     return StdStats.mean(arraySta);
    }
    public double stddev()
    {                   // sample standard deviation of percolation threshold
-	   return StdStats.stddev(count_array);
+     return StdStats.stddev(arraySta);
    }
    public double confidenceLo()
    {             // returns lower bound of the 95% confidence interval
-	   return StdStats.mean(count_array)-1.96*StdStats.stddev(count_array)/Math.sqrt(ipt_T);
+     return StdStats.mean(arraySta) 
+       - 1.96*StdStats.stddev(arraySta)/Math.sqrt(ipt);
    }
    public double confidenceHi()
    {             // returns upper bound of the 95% confidence interval
-	   return StdStats.mean(count_array)+1.96*StdStats.stddev(count_array)/Math.sqrt(ipt_T);
+     return StdStats.mean(arraySta) 
+       + 1.96*StdStats.stddev(arraySta)/Math.sqrt(ipt);
    }
-   public static void main(String[] args){
-	   // test client, described below
+   public static void main(String[] args) {
+     // test client, described below
    }
 }
